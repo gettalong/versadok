331e99f82829cb57910e890efdf10a38ee60419e
Thomas Leitner <t_leitner@gmx.at>
2025-05-13 00:21:02 +0200

    Add basic PDF renderer

---
+++ lib/versadok/pdf_renderer.rb
@@ -0,0 +1,280 @@
+# -*- encoding: utf-8; frozen_string_literal: true -*-
+#
+#--
+# This file is part of VersaDok.
+#
+# VersaDok - Versatile document creation markup and library
+# Copyright (C) 2025 Thomas Leitner <t_leitner@gmx.at>
+#
+# Permission is hereby granted, free of charge, to any person obtaining a
+# copy of this software and associated documentation files (the
+# "Software"), to deal in the Software without restriction, including
+# without limitation the rights to use, copy, modify, merge, publish,
+# distribute, sublicense, and/or sell copies of the Software, and to
+# permit persons to whom the Software is furnished to do so, subject to
+# the following conditions:
+#
+# The above copyright notice and this permission notice shall be included
+# in all copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+# OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+# IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY
+# CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,
+# TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+#++
+
+require 'erb'
+require 'yaml'
+require_relative 'renderer'
+require_relative 'utils'
+require_relative 'data_dir'
+require 'hexapdf'
+
+module VersaDok
+
+  # The PDF renderer takes a VersaDok AST and creates a PDF document using HexaPDF.
+  #
+  # See: HexaPDF - https://hexapdf.gettalong.org
+  #
+  # == Usage
+  #
+  # The easiest use is by calling #render with just the root Node. This returns a HexaPDF::Composer
+  # instance that has been used to create the document. Styling of the document is done via themes
+  # (either a predefined one or a custom one).
+  #
+  # If something more advanced is needed, it is possible to pass a HexaPDF::Document::Layout
+  # instance to the #render method. If this is done, no styling is performed and the result of the
+  # method is either a single HexaPDF box instance or an array of them (depending on the node passed
+  # in). This allows for integrating multiple separate ASTs into a single output document.
+  #
+  # == Styling
+  #
+  # Styling is done via a theme file in YAML format. Each key describes a style name and the value
+  # is a hash of HexaPDF::Layout::Style properties. In contrast to other styling systems like CSS
+  # there is no built-in inheritance: Only one style is applied. However, the :base key can be used
+  # to specify the base style from which style properties should be copied.
+  #
+  # Each unique node type (see Node#unique_type) has an associated default style. For example, a
+  # paragraph is styled via the "paragraph" style and a level 3 header via the "header3" style.
+  #
+  # It is also possible to apply specific styling based on the block node nesting. For example, if a
+  # paragraph is nested inside a blockquote, the "blockquote_paragraph" style would be applied
+  # instead of the "paragraph" style.
+  #
+  # Additionally, the text content of a block element can be styled by appending "_text" to the
+  # block style name, e.g. "paragraph_text", "header3_text" or "blockquote_paragraph_text".
+  class PDFRenderer < Renderer
+
+    using Utils::HashDeepMerge
+
+    def initialize(*) #:nodoc:
+      super
+      @stack = []
+      @stack_unique_types = []
+      @style_overrides = []
+    end
+
+    # Renders the given +node+ into a PDF document.
+    #
+    # There are two modes:
+    #
+    # 1. If +layout+ is not specified, a HexaPDF::Composer instance is created and #set_up_styles is
+    #    called. Then the node tree is rendered using this composer instance. Finally, the composer
+    #    instance is returned.
+    #
+    # 2. If +layout+ is specified, #set_up_styles is not called. The +layout+ object is used for
+    #    rendering the node tree and the result (either a single HexaPDF:::Layout::Box object or an
+    #    array of such boxes) is returned.
+    #
+    # In both cases the PDF document itself is not written, this is the job of the caller.
+    def render(node, layout: nil)
+      if layout
+        @layout = layout
+      else
+        @composer = HexaPDF::Composer.new(skip_page_creation: true)
+        @layout = @composer.document.layout
+        set_up_styles(@composer.document)
+        @composer.page_style(:default, page_size: :A4, orientation: :portrait) do |canvas, style|
+          style.frame = style.create_frame(canvas.context, 36)
+        end
+      end
+
+      @stack.clear
+      @stack_unique_types.clear
+      @style_overrides.clear
+      result = if node.type == :root
+                 render_root(node)
+               else
+                 super(node)
+               end
+
+      if layout
+        result
+      else
+        @composer.new_page
+        Array(result).each {|box| @composer.draw_box(box) }
+        @composer
+      end
+    end
+
+    # Sets up the styles needed for rendering in the given HexaPDF::Document.
+    #
+    # This uses the built-in default styling of the elements.
+    def set_up_styles(document)
+      theme = ERB.new(File.read(File.join(VersaDok.data_dir, 'themes', 'default.yaml'))).result
+      theme = YAML.safe_load(theme, permitted_classes: [Symbol], symbolize_names: true)
+      document.layout.styles(**theme)
+    end
+
+    private
+
+    # Renders the given +node+.
+    #
+    # Before the node is rendered, it is pushed onto the stack and afterwards it is removed from the
+    # stack. This allows using the node hierarchy for decisions.
+    def render_node(node)
+      @stack.push(node)
+      @stack_unique_types.push(node.unique_type)
+      result = super
+      @stack_unique_types.pop
+      @stack.pop
+      result
+    end
+
+    # Renders a paragraph via HexaPDF's +formatted_text+ helper.
+    def render_paragraph(para)
+      @layout.formatted_text(render_children(para), box_style: block_style)
+    end
+
+    # Renders a header via HexaPDF's +formatted_text+ helper.
+    def render_header(header)
+      @layout.formatted_text(render_children(header), box_style: block_style)
+    end
+
+    # Renders a blockquote by putting all child nodes into a container for styling.
+    def render_blockquote(blockquote)
+      @layout.container(splitable: true, style: block_style) do |container|
+        render_children(blockquote) {|result| container << result }
+      end
+    end
+
+    # Renders a list via HexaPDF's built-in list box.
+    def render_list(list)
+      @layout.list(start_number: list[:start], style: block_style) do |container|
+        render_children(list) {|result| container << result }
+      end
+    end
+
+    # Returns a hash that styles the given +text+.
+    #
+    # The return value is then used as item in the array passed to HexaPDF's +formatted_text+
+    # helper.
+    def render_text(text)
+      {text: text.content, style: text_style}
+    end
+
+    # Returns a single space character.
+    #
+    # See: #render_text
+    def render_soft_break(_node)
+      " "
+    end
+
+    # Returns a line feed character.
+    #
+    # See: #render_text
+    def render_hard_break(_node)
+      "\n"
+    end
+
+    # Returns a hash that styles the given +verbatim_text+ as verbatim.
+    #
+    # See: #render_text
+    def render_verbatim(verbatim_text)
+      {text: verbatim_text.content, style: text_style}
+    end
+
+    ['span', 'strong', 'emphasis', 'subscript', 'superscript'].each do |node_type|
+      define_method("render_#{node_type}") do |node|
+        render_children(node)
+      end
+    end
+
+    # Sets a style override for the link itself and returns the rendered children.
+    def render_link(link)
+      if link[:destination]
+        @style_overrides.push(overlays: {link: {uri: link[:destination]}})
+      else
+        #TODO
+      end
+      result = render_children(link)
+      @style_overrides.pop if link[:destination]
+      result
+    end
+
+    # Returns the appropriate HexaPDF::Layout::Style instance for the current node which needs to be
+    # a block node.
+    #
+    # See the class documentation for details.
+    def block_style
+      full_style_name = @stack_unique_types.join('_').intern
+      if @layout.style?(full_style_name)
+        @layout.style(full_style_name)
+      else
+        @layout.style(full_style_name, base: @stack_unique_types.last)
+      end
+    end
+
+    # Returns the appropriate HexaPDF::Layout::Style instance for the current text-based node.
+    def text_style
+      full_style_name = @stack_unique_types.join('_').intern
+
+      if @layout.style?(full_style_name)
+        style = @layout.style(full_style_name)
+      else
+        block_index = @stack.rindex {|node| node.category == :block } || 0
+        block_style_name = "#{@stack_unique_types[0..block_index].join('_')}_text".intern
+        parent_style = if @layout.style?(block_style_name)
+                         block_style_name
+                       else
+                         block_style_name = "#{@stack_unique_types[block_index]}_text".intern
+                         if @layout.style?(block_style_name)
+                           block_style_name
+                         else
+                           :base_text
+                         end
+                       end
+        style = @layout.style(full_style_name, base: parent_style)
+        @stack_unique_types[(block_index + 1)..-1].each do |unique_type|
+          style.merge(@layout.style(unique_type)) if @layout.style?(unique_type)
+        end
+      end
+
+      unless @style_overrides.empty?
+        style = style.dup
+        @style_overrides.each {|hash| style.update(**hash) }
+      end
+
+      style
+    end
+
+    # Renders the children of the +node+.
+    #
+    # If a block is given, each child node is rendered via #render_node and the result yielded.
+    #
+    # If no block is given, the results of calling #render_node for each child are put into a flat
+    # array and this array is returned.
+    def render_children(node)
+      if block_given?
+        node.children.each {|child| yield(render_node(child)) }
+      else
+        node.children.flat_map {|child| render_node(child) }
+      end
+    end
+
+  end
+
+end
---
+++ test/versadok/test_pdf_renderer.rb
@@ -0,0 +1,216 @@
+require 'test_helper'
+require 'versadok/pdf_renderer'
+require 'versadok/context'
+require 'versadok/node'
+require 'hexapdf/test_utils'
+
+describe VersaDok::PDFRenderer do
+  def node(type, children: nil, attr: nil, content: nil, **properties)
+    VersaDok::Node.new(type, content: content, attributes: attr, properties: properties).tap do |n|
+      n.children.replace(children) if children
+    end
+  end
+
+  def render(type, children: nil, attr: nil, content: nil, **properties)
+    @renderer.render(node(type, children: children, content: content, attr: attr, **properties),
+                     layout: @composer.document.layout)
+  end
+
+  before do
+    @context = VersaDok::Context.new
+    @renderer = VersaDok::PDFRenderer.new(@context)
+    @composer = HexaPDF::Composer.new
+  end
+
+  describe "render" do
+    it "creates a HexaPDF::Composer object if the layout argument is not provided" do
+      composer = @renderer.render(node(:root, children: [
+                                         node(:paragraph, children: [node(:text, content: 'Test')])]))
+      assert_kind_of(HexaPDF::Composer, composer)
+      assert_equal([0, 0, 595, 841], composer.document.pages[0].box(:media).value.map(&:to_i))
+      assert(composer.style?(:header2))
+    end
+  end
+
+  describe "block_style" do
+    it "allows specifiying nested styles" do
+      @composer.style(:blockquote_paragraph, font_size: 50)
+      box = render(:blockquote, children: [node(:paragraph)])
+      assert_equal(50, box.children[0].style.font_size)
+    end
+  end
+
+  describe "render_paragraph" do
+    it "uses a TextBox with style :paragraph" do
+      @composer.style(:paragraph, font_size: 50)
+      box = render(:paragraph)
+      assert_kind_of(HexaPDF::Layout::TextBox, box)
+      assert_equal(50, box.style.font_size)
+    end
+
+    it "renders the children" do
+      box = render(:paragraph, children: [node(:text, content: "Test")])
+      items = box.instance_variable_get(:@items)
+      assert_equal(1, items.size)
+      assert_equal("Test", box.text)
+    end
+  end
+
+  describe "render_header" do
+    it "uses a TextBox with style :headerLEVEL" do
+      @composer.style(:header2, font_size: 50)
+      box = render(:header, level: 2)
+      assert_kind_of(HexaPDF::Layout::TextBox, box)
+      assert_equal(50, box.style.font_size)
+    end
+
+    it "renders the children" do
+      @composer.style(:header2_text, font_size: 50)
+      box = render(:header, level: 2, children: [node(:text, content: "Test")])
+      items = box.instance_variable_get(:@items)
+      assert_equal(1, items.size)
+      assert_equal(50, items[0].style.font_size)
+    end
+  end
+
+  describe "render_blockquote" do
+    it "uses a splitable ContainerBox with style :blockquote" do
+      @composer.style(:blockquote, margin: 20)
+      box = render(:blockquote)
+      assert_kind_of(HexaPDF::Layout::ContainerBox, box)
+      assert(box.splitable)
+      assert_equal(20, box.style.margin.top)
+    end
+
+    it "renders the children" do
+      box = render(:blockquote, children: [node(:paragraph, children: [node(:text, content: "Test")])])
+      assert_equal(1, box.children.size)
+      assert_equal("Test", box.children[0].text)
+    end
+  end
+
+  describe "render_list" do
+    it "uses a ListBox with an appropriate style like :list_asterisk" do
+      @composer.style(:list_asterisk, margin: 20)
+      box = render(:list, marker: :asterisk)
+      assert_kind_of(HexaPDF::Layout::ListBox, box)
+      assert_equal(20, box.style.margin.top)
+    end
+
+    it "renders the children" do
+      item1 = node(:list_item, children: [node(:paragraph, children: [node(:text, content: "Test1")])])
+      item2 = node(:list_item, children: [node(:paragraph, children: [node(:text, content: "Test2")])])
+      box = render(:list, children: [item1, item2])
+      assert_equal(2, box.children.size)
+      assert_equal(1, box.children[0].size)
+      assert_equal("Test1", box.children[0][0].text)
+      assert_equal(1, box.children[1].size)
+      assert_equal("Test2", box.children[1][0].text)
+    end
+  end
+
+  describe "text_style" do
+    def check_style(**style_names)
+      box = render(
+        :blockquote,
+        children: [
+          node(:paragraph,
+               children: [node(:strong,
+                               children: [node(:emphasis, children: [node(:text, content: "Test")])])])
+        ])
+      style = box.children[0].instance_variable_get(:@items)[0].style
+      style_names.each {|name, value| assert_equal(value, style.send(name)) }
+    end
+
+    it "uses the fully nested style name if defined" do
+      @composer.style(:blockquote_paragraph_strong_emphasis_text, font_size: 50)
+      check_style(font_size: 50)
+    end
+
+    it "uses the nested block style name if the fully nested style name is not defined" do
+      @composer.style(:blockquote_paragraph_text, font_size: 50)
+      check_style(font_size: 50)
+    end
+
+    it "uses the inner-most block node type if a more specific name is not defined" do
+      @composer.style(:paragraph_text, font_size: 50)
+      check_style(font_size: 50)
+    end
+
+    it "falls back to the :base_text style name if no block-level style name is defined" do
+      @composer.style(:base_text, font_size: 50)
+      check_style(font_size: 50)
+    end
+
+    it "merges inline styles of nested nodes" do
+      @composer.style(:strong, font_size: 30, font_bold: true)
+      @composer.style(:emphasis, font_size: 20)
+      @composer.style(:base_text, font_size: 50, font_italic: true)
+      check_style(font_size: 20, font_bold: true, font_italic: true)
+    end
+
+    it "caches a created style for re-use" do
+      @composer.style(:paragraph_text, font_size: 50)
+      check_style(font_size: 50)
+      @composer.style(:paragraph_text, font_size: 100)
+      check_style(font_size: 50)
+    end
+
+    it "duplicates the resulting style if overrides are used" do
+      @composer.style(:paragraph_link_text, font_size: 50)
+      ['dest1.html', 'dest2.html'].each do |destination|
+        box = render(:paragraph, children: [node(:link, destination: destination, children: [
+                                                   node(:text, content: "Test")])])
+        assert_equal(50, box.instance_variable_get(:@items)[0].style.font_size)
+        assert_equal([[:link, {uri: destination}]],
+                     box.instance_variable_get(:@items)[0].style.overlays.layers)
+      end
+    end
+  end
+
+  describe "render_text" do
+    it "returns a hash with an appropriate style" do
+      @composer.style(:base_text, font_size: 20)
+      hash = render(:text, content: "Test")
+      assert_equal("Test", hash[:text])
+      assert_equal(20, hash[:style].font_size)
+    end
+  end
+
+  describe "soft_break" do
+    it "returns a space character" do
+      assert_equal(' ', render(:soft_break))
+    end
+  end
+
+  describe "hard_break" do
+    it "returns a line feed character" do
+      assert_equal("\n", render(:hard_break))
+    end
+  end
+
+  describe "render_verbatim" do
+    it "returns a hash with an appropriate style" do
+      @composer.style(:verbatim, font_size: 20)
+      hash = render(:verbatim, content: "Test")
+      assert_equal("Test", hash[:text])
+      assert_equal(20, hash[:style].font_size)
+    end
+  end
+
+  describe "render_link" do
+    it "applies a style override for inline links" do
+      result = render(:link, destination: 'dest.html', children: [node(:text, content: 'Test')])
+      assert_equal(1, result.size)
+      assert_equal("Test", result[0][:text])
+      assert_equal([[:link, {uri: 'dest.html'}]], result[0][:style].overlays.layers)
+    end
+
+    it "does nothing for reference links" do
+      result = render(:link, reference: 'ref', children: [node(:text, content: 'Test')])
+      assert_equal(1, result.size)
+      assert_equal("Test", result[0][:text])
+      assert_equal([], result[0][:style].each_property.to_a)
+    end
+  end
+end
